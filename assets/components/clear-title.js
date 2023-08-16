e41272059f3c512799d50610186467c0a4b9ffcf
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-08-16 12:43:47 +0200

    remove links in title

--- assets/components/clear-title.js
+++ assets/components/clear-title.js
@@ -7,7 +7,7 @@ class ClearTitleComponent extends HTMLElement {
       // Clear titles after a delay of 2 seconds (2000 milliseconds)
       const delay = 2000;
       setTimeout(() => {
-        const elementsWithMatchingTitle = document.querySelectorAll('[title="title"], [title*="&lt;"]');
+        const elementsWithMatchingTitle = document.querySelectorAll('[title="title"], [title*="&lt;"], [title*="https://"], [title*="http://"]');
         elementsWithMatchingTitle.forEach(element => {
           element.setAttribute('title', '');
         });
