ddea165ea94f6bfc9d5308bfd82fd5f1318bfc06
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-09-07 11:53:40 +0200

    improve title clearing

--- assets/components/clear-title.js
+++ assets/components/clear-title.js
@@ -2,16 +2,20 @@ class ClearTitleComponent extends HTMLElement {
     constructor() {
       super();
     }
+
+    clearTitles() {
+      console.log("Hello")
+      const elementsWithMatchingTitle = document.querySelectorAll('[title="title"], [title*="&lt;"], [title*="https://"], [title*="http://"]');
+        elementsWithMatchingTitle.forEach(element => {
+        element.setAttribute('title', '');
+      });
+    }
   
     connectedCallback() {
       // Clear titles after a delay of 2 seconds (2000 milliseconds)
       const delay = 2000;
-      setTimeout(() => {
-        const elementsWithMatchingTitle = document.querySelectorAll('[title="title"], [title*="&lt;"], [title*="https://"], [title*="http://"]');
-        elementsWithMatchingTitle.forEach(element => {
-          element.setAttribute('title', '');
-        });
-      }, delay);
+      this.clearTitles();
+      setTimeout(this.clearTitles, delay);
     }
   }
   
